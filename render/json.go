bed2aab65416db947d1fa42ba6f6faaaedbe7106
Dylan Johnston <qdylanj@gmail.com>
2018-02-10 13:42:41 +0100

    move render helpers to own package

---
+++ render/json.go
@@ -0,0 +1,30 @@
+package render
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type errorMessage struct {
+	Message string `json:"message"`
+}
+
+func JSON(w http.ResponseWriter, o interface{}) {
+	b, err := json.Marshal(o)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		Error(w, "failed to created json")
+		return
+	}
+
+	w.Write(b)
+}
+
+func Error(w http.ResponseWriter, m string) {
+	e := errorMessage{
+		Message: m,
+	}
+
+	JSON(w, e)
+}
