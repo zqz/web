95d6e26ef56b066440c98c11a874876228509a8b
Dylan Johnston <qdylanj@gmail.com>
2018-02-12 21:30:45 +0100

    reorganize filedb.go

--- filedb/filedb.go
+++ filedb/filedb.go
@@ -7,26 +7,130 @@ import (
 	"io"
 )
 
+// Persister reads and writes data.
 type Persister interface {
 	Put(string) (io.WriteCloser, error)
 	Get(string) (io.ReadCloser, error)
 }
 
+// Metastorer reads and writes Meta data.
 type MetaStorer interface {
 	FetchMeta(string) (*Meta, error)
 	StoreMeta(Meta) error
 }
 
+// FileDB implements a upload server.
 type FileDB struct {
 	p Persister
 	m MetaStorer
 }
 
+// NewFileDB returns an instance of a FileDB.
 func NewFileDB(p Persister, m MetaStorer) FileDB {
 	return FileDB{
 		m: m,
 		p: p,
 	}
+}
+
+func (db FileDB) Write(hash string, rc io.ReadCloser) (*Meta, error) {
+	if err := db.validate(); err != nil {
+		return nil, err
+	}
+
+	m, err := db.FetchMeta(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.store(m, rc); err != nil {
+		return m, err
+	}
+
+	if err := db.finish(m); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (db FileDB) Read(hash string, wc io.Writer) error {
+	if err := db.validate(); err != nil {
+		return err
+	}
+
+	reader, err := db.p.Get(hash)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(wc, reader)
+
+	return err
+}
+
+func (db FileDB) StoreMeta(meta Meta) error {
+	if err := db.validate(); err != nil {
+		return err
+	}
+
+	if err := validateMeta(&meta); err != nil {
+		return err
+	}
+
+	meta.BytesReceived = 0
+
+	m, _ := db.m.FetchMeta(meta.Hash)
+	if m != nil {
+		if meta.Size != m.Size {
+			return errors.New("can not change file size")
+		}
+
+		meta.BytesReceived = m.BytesReceived
+	}
+
+	return db.m.StoreMeta(meta)
+}
+
+func (db FileDB) FetchMeta(hash string) (*Meta, error) {
+	if err := db.validate(); err != nil {
+		return nil, err
+	}
+
+	if hash == "" {
+		return nil, errors.New("no hash specified")
+	}
+
+	return db.m.FetchMeta(hash)
+}
+
+func (db FileDB) validate() error {
+	if db.m == nil {
+		return errors.New("no storage specified")
+	}
+
+	if db.p == nil {
+		return errors.New("no persistence specified")
+	}
+
+	return nil
+}
+
+func validateMeta(meta *Meta) error {
+	if meta.Hash == "" {
+		return errors.New("no hash specified")
+	}
+
+	if meta.Size == 0 {
+		return errors.New("no size specified")
+	}
+
+	if meta.Name == "" {
+		return errors.New("no name specified")
+	}
+
+	return nil
 }
 
 func (db FileDB) finish(m *Meta) error {
@@ -74,106 +178,6 @@ func (db FileDB) store(m *Meta, rc io.ReadCloser) error {
 	}
 
 	return nil
-}
-
-func (db FileDB) Write(hash string, rc io.ReadCloser) (*Meta, error) {
-	if err := db.validate(); err != nil {
-		return nil, err
-	}
-
-	m, err := db.FetchMeta(hash)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := db.store(m, rc); err != nil {
-		return m, err
-	}
-
-	if err := db.finish(m); err != nil {
-		return nil, err
-	}
-
-	return m, nil
-}
-
-func (db FileDB) Read(hash string, wc io.Writer) error {
-	if err := db.validate(); err != nil {
-		return err
-	}
-
-	reader, err := db.p.Get(hash)
-
-	if err != nil {
-		return err
-	}
-
-	_, err = io.Copy(wc, reader)
-
-	return err
-}
-
-func (db FileDB) validate() error {
-	if db.m == nil {
-		return errors.New("no storage specified")
-	}
-
-	if db.p == nil {
-		return errors.New("no persistence specified")
-	}
-
-	return nil
-}
-
-func validateMeta(meta *Meta) error {
-	if meta.Hash == "" {
-		return errors.New("no hash specified")
-	}
-
-	if meta.Size == 0 {
-		return errors.New("no size specified")
-	}
-
-	if meta.Name == "" {
-		return errors.New("no name specified")
-	}
-
-	return nil
-}
-
-func (db FileDB) StoreMeta(meta Meta) error {
-	if err := db.validate(); err != nil {
-		return err
-	}
-
-	if err := validateMeta(&meta); err != nil {
-		return err
-	}
-
-	meta.BytesReceived = 0
-
-	m, _ := db.m.FetchMeta(meta.Hash)
-	if m != nil {
-		if meta.Size != m.Size {
-			return errors.New("can not change file size")
-		}
-
-		meta.BytesReceived = m.BytesReceived
-	}
-
-	return db.m.StoreMeta(meta)
-}
-
-func (db FileDB) FetchMeta(hash string) (*Meta, error) {
-	if err := db.validate(); err != nil {
-		return nil, err
-	}
-
-	if hash == "" {
-		return nil, errors.New("no hash specified")
-	}
-
-	return db.m.FetchMeta(hash)
 }
 
 func calcHash(src io.Reader) (string, error) {
