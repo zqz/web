2105125570a1ae401a331a99c6177fa365f4f4e9
Dylan Johnston <qdylanj@gmail.com>
2018-04-09 18:29:41 +0200

    serve index

--- serv.go
+++ serv.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"fmt"
+	"html/template"
 	"net/http"
+	"strings"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 	_ "github.com/lib/pq"
 	"github.com/zqz/upl/filedb"
 )
@@ -38,6 +44,89 @@ func connect() (*sql.DB, error) {
 	return db, nil
 }
 
+func fs(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		return
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	var css, js []string
+	var err error
+
+	assets := assetFS()
+
+	css, err = assets.AssetDir("build/static/css")
+	if err != nil {
+		panic(err)
+	}
+	js, err = assets.AssetDir("build/static/js")
+	if err != nil {
+		panic(err)
+	}
+
+	tmplData := map[string]interface{}{
+		"WSPath":  template.JSStr('/'),
+		"ApiRoot": template.JSStr(fmt.Sprintf("http://%s/api", r.Host)),
+
+		"Assets": map[string]interface{}{
+			"Js":  js,
+			"Css": css,
+		},
+	}
+
+	tmplContent := `
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta http-equiv='content-type' content='text/html; charset=utf-8'>
+    <title>zqz.ca</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+    <link rel='shortcut icon' href='/assets/favicon.ico'/>
+    {{- range .Assets.Css }}
+    <link rel='stylesheet' media='screen' href='/assets/static/css/{{ . }}'/>
+    {{- end }}
+  </head>
+  <body>
+    <script type='text/javascript'>
+      window.apiRoot = {{.ApiRoot}};
+    </script>
+  	<noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div>
+
+    {{- range .Assets.Js }}
+    <script type='text/javascript' src='/assets/static/js/{{.}}'></script>
+    {{- end }}
+  </body>
+</html>`
+
+	t := template.New("App Index Template")
+	t, err = t.Parse(tmplContent)
+	if err != nil {
+		panic(err)
+	}
+
+	var output bytes.Buffer
+	err = t.Execute(&output, tmplData)
+	if err != nil {
+		panic(err)
+	}
+
+	render.HTML(w, r, output.String())
+}
+
 func main() {
 	// os.Mkdir(tmpPath, 0744)
 	// os.Mkdir(finalPath, 0744)
@@ -58,5 +147,11 @@ func main() {
 
 	s.EnableLogging = true
 
-	http.ListenAndServe(":3001", s.Router())
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Mount("/api", s.Router())
+	fs(r, "/assets", assetFS())
+	r.Get("/*", serveIndex)
+
+	http.ListenAndServe(":3001", r)
 }
