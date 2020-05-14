ead12c07087df3146ec464d9d602f55e5893006a
Dylan Johnston <qdylanj@gmail.com>
2020-05-14 23:22:01 +0200

    Format rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -17,136 +17,46 @@ export default {
   output: {
     sourcemap: true,
     name: 'app',
-		file: 'public/build/bundle.js',
+    file: 'public/build/bundle.js',
     format: 'iife'
   },
   plugins: [
     svelte({
-      // You can restrict which files are compiled
-      // using `include` and `exclude`
       include: 'src/**/*.svelte',
-
-      // By default, the client-side compiler is used. You
-      // can also use the server-side rendering compiler
-      // generate: 'ssr',
-      
-      // ensure that extra attributes are added to head
-      // elements for hydration (used with ssr: true)
       hydratable: true,
-
-      // Optionally, preprocess components with svelte.preprocess:
-      // https://svelte.dev/docs#svelte_preprocess
-      // preprocess: {
-      //   style: ({ content }) => {
-      //     return transformStyles(content);
-      //   }
-      // },
       preprocess: autoPreprocess(),
-
-      // Emit CSS as "files" for other plugins to process
-      // emitCss: true,
-
-      // Extract CSS into a separate file (recommended).
-      // See note below
       css: function (css) {
-        console.log(css.code); // the concatenated CSS
-        console.log(css.map); // a sourcemap
-
-        // creates `main.css` and `main.css.map` — pass `false`
-        // as the second argument if you don't want the sourcemap
         css.write('public/build/main.css');
       },
 
-      // Warnings are normally passed straight to Rollup. You can
-      // optionally handle them here, for example to squelch
-      // warnings with a particular code
       onwarn: (warning, handler) => {
-        // e.g. don't warn on <marquee> elements, cos they're cool
-        if (warning.code === 'a11y-distracting-elements') return;
-
-        // let Rollup handle all other warnings normally
         handler(warning);
       }
     }),
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
+    resolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
+    commonjs(),
     !production && livereload({watch: 'public'}),
-		production && terser(),
+    production && terser(),
     !production && serve()
   ]
-}
-
-/*
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-
-const production = false;// !process.env.ROLLUP_WATCH;
-
-export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
-		}),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-    !production && livereload({watch: 'public'}),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
 };
-*/
+
 function serve() {
-	let started = false;
+  let started = false;
 
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
+        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+          stdio: ['ignore', 'inherit', 'inherit'],
+          shell: true
+        });
+      }
+    }
+  };
 }
