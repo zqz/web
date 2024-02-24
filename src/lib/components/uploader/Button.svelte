942b600c0acbe63107696ac4936816b4f95800e8
Dylan Johnston <qdylanj@gmail.com>
2024-02-25 00:07:24 +0100

    chore: remove unused file

--- src/lib/components/uploader/Button.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  export let title: string;
-  export let className: string;
-</script>
-
-<button aria-label={title} title={title} class="btn {className}" on:click>
-  <slot></slot>
-</button>
