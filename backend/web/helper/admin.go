90942267232e78ee833ab3ff39f0c1fd8c87a8d2
Dylan Johnston <qdylanj@gmail.com>
2025-05-25 22:38:03 +0200

    chore: add back fake admin check

--- backend/web/helper/admin.go
+++ backend/web/helper/admin.go
@@ -3,6 +3,5 @@ package helper
 import "github.com/zqz/web/backend/userdb"
 
 func IsAdmin(u *userdb.User) bool {
-	return false
-	// return u != nil && (u.Email == "dylan@johnston.ca" || u.Email == "qdylanj@gmail.com")
+	return u != nil && (u.Email == "dylan@johnston.ca" || u.Email == "qdylanj@gmail.com")
 }
