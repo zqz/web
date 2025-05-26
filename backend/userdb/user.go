4de8263d32b574cfb606a8c708eb0910d013ab72
Dylan Johnston <qdylanj@gmail.com>
2025-05-26 21:25:46 +0200

    chore: rename

--- backend/userdb/user.go
+++ backend/userdb/user.go
@@ -1,56 +1,11 @@
 package userdb
 
 import (
-	"errors"
-	"strings"
-
 	"github.com/zqz/web/backend/models"
 )
 
 type User struct {
 	models.User
-}
-
-type DB struct {
-	p persister
-}
-
-type persister interface {
-	Create(*User) error
-	FindByProviderId(string) (*User, error)
-	FindById(int) (*User, error)
-	Update(int, User) (User, bool, error)
-	List() ([]*User, error)
-}
-
-func NewDB(p persister) DB {
-	return DB{
-		p: p,
-	}
-}
-
-func (db DB) FindByProviderId(id string) (*User, error) {
-	return db.p.FindByProviderId(id)
-}
-
-func (db DB) FindById(id int) (*User, error) {
-	return db.p.FindById(id)
-}
-
-func (db DB) Create(u *User) error {
-	if u == nil {
-		return errors.New("a user is required")
-	}
-
-	if issues, ok := validUser(u); !ok {
-		return errors.New(strings.Join(issues, ", "))
-	}
-
-	return db.p.Create(u)
-}
-
-func (db DB) List() ([]*User, error) {
-	return db.p.List()
 }
 
 func validUser(u *User) ([]string, bool) {
