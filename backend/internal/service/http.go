362ad30a58f8347b844e70c8d0f08c199d9de282
Dylan Johnston <qdylanj@gmail.com>
2025-05-27 20:13:11 +0200

    chore: move gothic setup to http server

--- backend/internal/service/http.go
+++ backend/internal/service/http.go
@@ -4,6 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
@@ -54,8 +60,30 @@ func (s Server) runInsecure(r http.Handler) error {
 	listenPort := fmt.Sprintf(":%d", s.config.Port)
 
 	s.logger.Info().Int("port", s.config.Port).Msg("listening for requests")
+	s.setupGoth()
 
 	return http.ListenAndServe(listenPort, r)
+}
+
+func (s Server) setupGoth() {
+	key := "xyz"         // Replace with your SESSION_SECRET or similar
+	maxAge := 86400 * 30 // 30 days
+	isProd := false      // Set to true when serving over https
+
+	store := sessions.NewCookieStore([]byte(key))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true // HttpOnly should always be enabled
+	store.Options.Secure = isProd
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New(
+			os.Getenv("GOOGLE_KEY"),
+			os.Getenv("GOOGLE_SECRET"),
+			"http://localhost:3001/auth/callback?provider=google",
+		),
+	)
 }
 
 func (s Server) Run() error {
--- backend/internal/transport/web/router.go
+++ backend/internal/transport/web/router.go
@@ -5,14 +5,9 @@ import (
 	"net/http"
 	"strconv"
 
-	"os"
-
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-chi/chi/v5"
-	"github.com/gorilla/sessions"
-	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
-	"github.com/markbates/goth/providers/google"
 
 	"github.com/zqz/web/backend/internal/domain/file"
 	"github.com/zqz/web/backend/internal/domain/user"
@@ -21,24 +16,6 @@ import (
 )
 
 func DefaultRoutes(users *user.DB, db *file.FileDB) *chi.Mux {
-	key := "xyz"         // Replace with your SESSION_SECRET or similar
-	maxAge := 86400 * 30 // 30 days
-	isProd := false      // Set to true when serving over https
-
-	store := sessions.NewCookieStore([]byte(key))
-	store.MaxAge(maxAge)
-	store.Options.Path = "/"
-	store.Options.HttpOnly = true // HttpOnly should always be enabled
-	store.Options.Secure = isProd
-	gothic.Store = store
-
-	goth.UseProviders(
-		google.New(
-			os.Getenv("GOOGLE_KEY"),
-			os.Getenv("GOOGLE_SECRET"),
-			"http://localhost:3001/auth/callback?provider=google",
-		),
-	)
 	r := chi.NewRouter()
 
 	r.Get("/auth", func(w http.ResponseWriter, r *http.Request) {
